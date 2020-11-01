e545e3f4c5f58d42d7d40b12131170a86f3021a6
Kai Leddy <kai.leddy@azuri-technologies.com>
2020-11-01 20:37:39 +0000

    Make the cells toggle-able

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Grid from './components/Grid';
+import { toggleCell } from './utils';
 
-const emptyGrid: boolean[][] = Array(10).fill(Array(10).fill(0));
+const emptyGrid: boolean[][] = Array(10).fill(Array(10).fill(false));
 
 const App = () => {
   const [tick, setTick] = useState(0);
   const [grid, setGrid] = useState(emptyGrid);
 
+  const onToggleCell = useCallback((y, x) => {
+    setGrid((g) => toggleCell(g, y, x));
+  }, []);
+
   return (
     <main>
       <h1>Conway's Game of Life</h1>
-      <Grid grid={grid} />
+      <Grid grid={grid} onToggleCell={onToggleCell} />
       <br />
       <div>
         <button>Start</button>
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -3,14 +3,15 @@ import Cell from './Cell';
 
 type Props = {
   grid: boolean[][];
+  onToggleCell: (y: number, x: number) => void;
 };
-const Grid = ({ grid }: Props) => {
+const Grid = ({ grid, onToggleCell }: Props) => {
   return (
     <div>
-      {grid.map((y) => (
-        <div>
-          {y.map((x) => (
-            <Cell alive={x} />
+      {grid.map((row, y) => (
+        <div key={y}>
+          {row.map((cell, x) => (
+            <Cell key={x} alive={cell} onToggle={() => onToggleCell(y, x)} />
           ))}
         </div>
       ))}
